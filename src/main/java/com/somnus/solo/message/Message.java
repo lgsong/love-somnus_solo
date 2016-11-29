8999de6f80c7867b40cdfe59c536b8bb885518ae
somnus <928200207@qq.com>
2016-11-29 16:34:46 +0800

    添加时间的正则校验，不能随便传

--- src/main/java/com/somnus/solo/message/Message.java
+++ src/main/java/com/somnus/solo/message/Message.java
@@ -1,5 +1,7 @@
 package com.somnus.solo.message;
 
+import javax.validation.constraints.Pattern;
+
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -21,6 +23,10 @@ public class Message implements java.io.Serializable {
     
     /** 前置机时间*/
     @NotEmpty
+    @Pattern(regexp = "^(?:(?!0000)[0-9]{4}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-8])|"
+    		+ "(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[0-9]{2}(?:0[48]|[2468][048]|"
+    		+ "[13579][26])|(?:0[48]|[2468][048]|[13579][26])00)-02-29)\\s+([01][0-9]|"
+    		+ "2[0-3]):[0-5][0-9]:[0-5][0-9]$",message = "must be yyyy-MM-dd HH:mm:ss" )
     @JsonInclude(Include.NON_EMPTY)
     private String frontTime;
     
